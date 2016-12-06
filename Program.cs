ca9a563de8b75c245298bd16093089cd619e211b
yourname <youremail@address.com>
2016-12-06 18:12:12 +0000

    Project Start
    Initialised project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and Welcome. \n\nPlease type the name of the project you'd like to start: e.g. 'Living Room' or 'Kitchen'")
+            string projectName = Console.R
+        }
+    }
+}
